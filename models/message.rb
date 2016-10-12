c00df3911df5b9e72ffe23b88b4ac6edc7d4407c
Leonardo Mateo <leonardomateo@gmail.com>
2016-10-11 22:54:39 -0300

    Sort results by creation date

--- models/message.rb
+++ models/message.rb
@@ -1,5 +1,5 @@
 class Message < Sequel::Model
   def self.search(options = { language: "%", limit: 100 })
-    where("language LIKE (?)", "%#{options[:language]}%").limit(options[:limit])
+    where("language LIKE (?)", "%#{options[:language]}%").order(Sequel.desc(:created_at)).limit(options[:limit])
   end
 end
