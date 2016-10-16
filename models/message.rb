42049cb479bced28896937fc2299f2cdb1249fd6
Leonardo Mateo <leonardomateo@gmail.com>
2016-10-16 15:05:41 -0300

    Improve messages date time hash representation

--- models/message.rb
+++ models/message.rb
@@ -2,4 +2,8 @@ class Message < Sequel::Model
   def self.search(options = { language: "%", limit: 100 })
     where("language LIKE (?)", "%#{options[:language]}%").order(Sequel.desc(:created_at)).limit(options[:limit])
   end
+
+  def to_hash
+    values.merge(created_at: created_at.iso8601, updated_at: updated_at.iso8601)
+  end
 end
